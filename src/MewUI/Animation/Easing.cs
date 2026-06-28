7ffd96b496f7a3b51d4efe5e2e988dc6aba6357b
Youngjae Song <al6uiz@gmail.com>
2026-06-28 14:57:48 +0900

    Change default easing to EaseInOutBack in Easing.Default

--- src/MewUI/Animation/Easing.cs
+++ src/MewUI/Animation/Easing.cs
@@ -9,7 +9,7 @@ public static class Easing
     /// <summary>
     /// The default easing function used by animations when none is specified.
     /// </summary>
-    public static readonly Func<double, double> Default = EaseOutCubic;
+    public static Func<double, double> Default { get; } = EaseOutCubic;
 
     public static double Linear(double t) => t;
 
